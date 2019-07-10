b7937b22640e5a5f3a02c5c2c8893aec8d548c22
busra <busrasimsek929@gmail.com>
2019-07-10 17:00:55 +0300

    Wheel and Slider

---
+++ Assets/Scripts/BarControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarControl : MonoBehaviour
+{
+
+    public Slider progressBar;
+    float maxVal = 100;
+    public float currentval = 0;
+    bool isInc = true; //yukarı mı aşağı mı gideceğimize karar veriyor 0 ise true oluyor ve yukarı cıkıyor, 100 ise false oluyor ve aşağı iniyor
+    bool defspeed = false; //durcak mı calıscak mı (true iken calısıyor false iken duruyor)
+    bool isCliked = false; //bir kere bastığımızda fonksiyonları devamlı olarak cagırmayı engelliyor. tutukluk yapmasını engelliyor
+    public bool speedef = false;
+
+     private void Start()
+     {
+         StartCoroutine("UpdateRoutine");
+     }
+
+     IEnumerator UpdateRoutine()
+     {
+         while (true)
+         {
+             if (Input.GetMouseButton(0) && !isCliked) 
+             {
+                 isCliked = true;
+                 if (!defspeed)
+                 {
+                     defspeed = true;
+                     StartCoroutine("GucBarPlay");
+
+                 }
+                 else
+                 {
+                     speedef = true;
+                     defspeed = false;
+                     StopCoroutine("GucBarPlay");
+                     StopCoroutine("UpdateRoutine");
+                 }
+             }
+             if(isCliked && !Input.GetMouseButton(0))
+             {
+                 isCliked = false;
+             }
+             yield return null;
+         }
+     }
+
+     IEnumerator GucBarPlay()
+     {
+         Debug.Log("progres bar artsın");
+         while (true)
+         {
+
+            if (isInc)
+             {
+                 currentval++;
+                 if (currentval == 100)
+                 {
+                     isInc = false;
+                 }
+             }
+
+             else
+             {
+                 currentval--;
+                 if (currentval == 0)
+                 {
+                     isInc = true;
+                 }
+             }
+            progressBar.value = currentval; //progress barı güncelliyoruz
+
+            yield return null;
+         } 
+
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float thrust = 0;
+    public Rigidbody rb;
+    bool walk = false;
+    private float speed;
+
+    [SerializeField] float dragforce;
+    [SerializeField] float massforce;
+    [SerializeField] int dragsecond;
+
+    //[SerializeField] private Slider slider;
+    [SerializeField] GameObject UIcontroller;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = massforce;
+    }
+
+    void Update()
+    {
+        if (UIcontroller.GetComponent<BarControl>().speedef)
+        {
+
+            speed = UIcontroller.GetComponent<BarControl>().currentval * thrust;
+            if (!walk)
+            {
+                rb.AddForce(Vector3.right * speed);
+                walk = true;
+             
+                StartCoroutine("surtunme");
+            }
+            else
+            {
+               // UIcontroller.GetComponent<BarControl>().progressBar.value = rb.velocity.magnitude;
+
+            }
+        }
+    }
+
+    IEnumerator surtunme()
+    {
+        yield return new WaitForSecondsRealtime(dragsecond);
+        rb.drag += dragforce;
+    }
+
+    
+}
